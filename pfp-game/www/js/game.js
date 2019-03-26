5ae73cf4acc91e3fbc397352da61cefaa9f65428
Benjamin Fele <benjamin.fele@gmail.com>
2019-03-26 14:43:16 +0100

    weapon loading bug fix

--- pfp-game/www/js/game.js
+++ pfp-game/www/js/game.js
@@ -32,7 +32,7 @@ function preload() {
         this.load.image("obstacle-" + i, "img/obstacle-" + i + ".png");
         this.load.spritesheet("enemy-" + i, "img/enemy-" + i + ".png", {frameWidth: 24, frameHeight: 24});
 
-        this.load.spritesheet("weapon-" + i, "img/weapon-" + i + ".png", {frameWidth: 2, frameHeight: 8});
+        this.load.image("weapon-" + i, "img/weapon-" + i + ".png", {frameWidth: 2, frameHeight: 8});
 
         this.load.spritesheet("helper-" + i, "img/helper-" + i + ".png", {frameWidth: 48, frameHeight: 64});
         this.load.spritesheet("boss-" + i, "img/boss-" + i + ".png", {frameWidth: 48, frameHeight: 64});
@@ -54,7 +54,7 @@ function preload() {
     this.load.image("background-placeholder", "img/background-placeholder.png");
     this.load.image("obstacle-placeholder", "img/obstacle-placeholder.png");
     this.load.spritesheet("enemy-placeholder", "img/enemy-placeholder.png", {frameWidth: 24, frameHeight: 24});
-    this.load.spritesheet("weapon-placeholder", "img/weapon-placeholder.png", {frameWidth: 2, frameHeight: 8});
+    this.load.image("weapon-placeholder", "img/weapon-placeholder.png", {frameWidth: 2, frameHeight: 8});
     this.load.spritesheet("helper-placeholder", "img/helper-placeholder.png", {frameWidth: 48, frameHeight: 64});
     this.load.spritesheet("boss-placeholder", "img/boss-placeholder.png", {frameWidth: 48, frameHeight: 64});
 }
@@ -205,7 +205,7 @@ function changeMode() {
 
         currModeInstance = new Story(prevModeInstance.environment);
         $(currModeInstance).attr(currModeInstance.scene.cache.json.get(newMode));
-        
+
         currModeInstance.initializeStory(prevModeInstance);
         currModeInstance.resumeGameplay(false, true);
     }
